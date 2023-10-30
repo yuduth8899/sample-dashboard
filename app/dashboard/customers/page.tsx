775365b9222c7be0aa2b9ecc71ca6407265d3138
rusli abdul gani <abdul.gani@fullertonhealth.com>
2023-10-30 20:34:01 +0700

    deploye

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,5 @@
+export default function Page() {
+  return (
+    <div>Customers page</div>
+  )
+}
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,7 @@
+import SideNav from "../ui/dashboard/sidenav"
+
+export default function Page() {
+  return (
+    <p>Dashboard page</p>
+  )
+}
